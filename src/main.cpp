2669a25fa982adbd747528195f0a8af8a80ea2f3
Otto Multanen <multanen.otto@gmail.com>
2021-08-10 19:29:12 +0300

    init simple branch

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
-    
-    // while (1) { } // Problematic with valgrind etc
+    std::cout << "Hello world" << std::endl;
+
     return 0;
 }
